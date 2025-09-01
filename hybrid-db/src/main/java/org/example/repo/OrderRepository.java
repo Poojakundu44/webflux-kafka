b6e7fecc3cb068d092089be57ce1261069e349ec
Poojakundu44 <35173553+Poojakundu44@users.noreply.github.com>
2025-09-01 14:52:17 +0530

    Delete hybrid-db directory

--- hybrid-db/src/main/java/org/example/repo/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.repo;
-
-import org.aspectj.weaver.ast.Or;
-import org.example.entity.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-
-public interface OrderRepository extends JpaRepository<Order ,Long> {
-}
--- hybrid-db/src/main/java/org/example/repo/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.repo;
-
-import org.example.document.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product ,Long> {
-}
